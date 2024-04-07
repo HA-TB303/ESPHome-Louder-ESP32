2a9d7253e1fa7fb76498854b301b21dbd4fbbbfa
espthings.io <9609805+HA-TB303@users.noreply.github.com>
2024-04-07 13:30:16 +0200

    Update louderesp32.h

--- louderesp32.h
+++ louderesp32.h
@@ -1,29 +1,48 @@
+//###########################################################################
+//## ESPHome custom component for the Louder ESP32                         ##
+//## Get it here: https://www.tindie.com/products/sonocotta/louder-esp32/  ##
+//## Check the blog article on https://www.espthings.io/louder-esp32       ##
+//###########################################################################
 #include "esphome.h"
+#include <Wire.h>
 
-const uint16_t I2C_ADDRESS = 0x21;
-const uint16_t REGISTER_ADDRESS = 0x78;
-const uint16_t POLLING_PERIOD = 15000; //milliseconds
-char temp = 20; //Initial value of the register
+#define DEVICE_CTRL_2_REGISTER 0x03
+#define PWDN_PIN 33
+#define I2C_ADDR 0x2D
 
-class MyCustomComponent : public PollingComponent {
- public:
-  MyCustomComponent() : PollingComponent(POLLING_PERIOD) {}
-  float get_setup_priority() const override { return esphome::setup_priority::BUS; } //Access I2C bus
+class TAS5805 : public Component, public Switch  {
+  public:
+    void setup() override {
+      pinMode(PWDN_PIN, OUTPUT);
+      digitalWrite(PWDN_PIN, LOW);
+      delay(200);
+      digitalWrite(PWDN_PIN, HIGH);
 
-  void setup() override {
-    //Add code here as needed
-    Wire.begin();
+      Wire.begin();
+      Wire.beginTransmission(I2C_ADDR);
+      if (Wire.endTransmission() != 0) {
+        ESP_LOGE("TAS5805", "TAS5805 not found at address 0x2D");
+        return;
+      }
+      Wire.beginTransmission(I2C_ADDR);
+      Wire.write(DEVICE_CTRL_2_REGISTER);
+      Wire.write(0x02);
+      Wire.endTransmission();
+      delay(50);
+      Wire.beginTransmission(I2C_ADDR);
+      Wire.write(DEVICE_CTRL_2_REGISTER);
+      Wire.write(0x03);
+      Wire.endTransmission();
+
+      ESP_LOGI("TAS5805", "TAS5805 initialized.");
     }
 
-  void update() override {
-  char register_value = id(input_1).state; //Read the number set on the dashboard
-  //Did the user change the input?
-  if(register_value != temp){
-        Wire.beginTransmission(I2C_ADDRESS);
-        Wire.write(REGISTER_ADDRESS);
-        Wire.write(register_value);
-        Wire.endTransmission();
-        temp = register_value; //Swap in the new value
-        }
+    void write_state(bool state) override {
+      uint8_t value = state ? 0x03 : 0x00;
+      Wire.beginTransmission(I2C_ADDR);
+      Wire.write(DEVICE_CTRL_2_REGISTER);
+      Wire.write(value);
+      Wire.endTransmission();
+      publish_state(state);
     }
 };
